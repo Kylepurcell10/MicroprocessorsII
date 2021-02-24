8daf1612720ea9530a9e436d319574d8b56db98e
Kylepurcell10 <73486286+Kylepurcell10@users.noreply.github.com>
2021-02-24 16:33:15 -0500

    Update to Lab 1
    Doesnt work just yet but could be a potentially successful structure

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -1,9 +1,43 @@
+int start = 13;
+int green = 2;
+int yellow = 3;
+int red = 4;
+int buzzer = 5;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(OUTPUT, green);
+  pinMode(OUTPUT, yellow);
+  pinMode(OUTPUT, red);
+  pinMode(OUTPUT, buzzer);
+  pinMode(INPUT, start);
+  while(start == LOW){
+    digitalWrite(red, LOW);
+    delay(1000);
+    digitalWrite(red, HIGH);
+  }
 }
-
+void buzzerFunction(){
+    digitalWrite(buzzer, HIGH);
+    delay(3000);
+    digitalWrite(buzzer, LOW);
+  }
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(start == HIGH){
+    while(true){
+    digitalWrite(red, HIGH);
+    delay(12000);
+    buzzerFunction();
+    digitalWrite(red, LOW);
+    digitalWrite(green, HIGH);
+    delay(12000);
+    buzzerFunction();
+    digitalWrite(green, LOW);
+    digitalWrite(yellow, HIGH);
+    buzzerFunction();
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, HIGH);
+    }
+  }   
 }
+ 
